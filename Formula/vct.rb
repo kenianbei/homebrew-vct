0ffdd4a287e44e7ef1152f85d5616fa0e7c253a4
kenianbei <kenianbei@gmail.com>
2023-01-31 19:52:24 +0700

    Update formula to 0.1.1.

--- Formula/vct.rb
+++ Formula/vct.rb
@@ -1,9 +1,9 @@
 class Vct < Formula
   desc "Terminal UI application for managing vCard contacts, written in Rust."
   homepage "https://github.com/kenianbei/vcard_tui"
-  url "https://github.com/kenianbei/vcard_tui/releases/download/0.1.0/vct.tar.gz"
-  sha256 "aeeaa2d6a3b53f6adb78b029e5c0eb1aecefe45a1bf3dedcfb21a81dfbad6135"
-  version "0.1.0"
+  url "https://github.com/kenianbei/vcard_tui/releases/download/0.1.1/vct.tar.gz"
+  sha256 "acd6454b997cbe538a5a206d732cc962f21a8595ffa4589f30584324c20b40e7"
+  version "0.1.1"
 
   def install
     bin.install "vct"
